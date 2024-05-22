b226d1feed401789e7b8bf8c479e59a8959d65a9
nikoslava-dev <156796210+nikoslava-dev@users.noreply.github.com>
2024-05-22 13:53:31 +0300

    Add files via upload

---
+++ AsyncDev/ProcessMultipleRequests.cs
@@ -0,0 +1,35 @@
+﻿namespace AsyncDev
+{
+    public class ProcessMultipleRequests
+    {
+        public async void RunProcess()
+        {
+            var urls = new List<string>
+            {
+                "https://www.naveedulhaq.com/index.php/dot-net-core/combining-async-and-yield-in-c/",
+                "https://docs.mapbox.com/mapbox-gl-js/api/",
+                "https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia"
+            };
+
+            await foreach (var content in AsyncRequests(urls))
+            {
+                // Process the content asynchronously
+                Console.WriteLine($"------------------------------------------------ {content}");
+            }
+
+            Console.WriteLine($"end process");
+        }
+
+        public async IAsyncEnumerable<string> AsyncRequests(IEnumerable<string> urls)
+        {
+            foreach (var url in urls)
+            {
+                using var client = new HttpClient();
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+
+                yield return content;
+            }
+        }
+    }
+}
---
+++ AsyncDev/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// --------------------------ASYNC VALUES---------------------------------------
+// Console.WriteLine("IEnumerbleApproach process start");
+// new IEnumerbleApproach().Main();
+
+// have to block to wait 10 sec for items
+// Console.ReadKey();
+
+// ---------------------------------------------------------------
+
+// Console.WriteLine("YieldApproach process start");
+// new YieldApproach().Main();
+
+// ---------------------------------------------------------------
+
+// Console.WriteLine("IAsyncEnumerableApproach process start");
+// new IAsyncEnumerableApproach().Main();
+
+// have to block to wait 10 sec for items
+// Console.ReadKey();
+
+// ---------------------------------------------------------------
+
+// -------------------------LARGE FILE---------------------------------------
+
+// new LargeFileReading().RunProcess();
+// Console.ReadKey();
+
+// -------------------------------------------------------------------------
+
+// -----------------------------Process Requests------------------------------------------------
+
+// new ProcessMultipleRequests().RunProcess();
+// Console.ReadKey();
+
+// -------------------------------------------------------------------------------
